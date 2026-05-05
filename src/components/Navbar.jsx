9e211bc8003c8273c035b3a4cfe8153a4e5e5bfa
Taron Bentley <taronbentley1@gmail.com>
2026-05-05 14:08:27 -0400

    Changed Title and links in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="logo">Hero Clash</h2>
+      <h2 className="logo">Multiverse Archive</h2>
 
       <div className="nav-links">
         <a href="#">Home</a>
-        <a href="#">Battle</a>
-        <a href="#">Favorites</a>
+        <a href="#">Battle Arena</a>
+        <a href='#'>Favorites</a>
       </div>
     </nav>
   );
