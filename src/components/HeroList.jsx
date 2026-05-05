ec6f86df950d4b002dd63edb08fec7f03ffda2ed
Taron Bentley <taronbentley1@gmail.com>
2026-05-05 13:10:04 -0400

    refactor HeroList component to pass onSelectHero prop to HeroCard

--- src/components/HeroList.jsx
+++ src/components/HeroList.jsx
@@ -1,10 +1,10 @@
 import HeroCard from "./HeroCard";
 
-function HeroList({ heroes }) {
+function HeroList({ heroes, onSelectHero }) {
   return (
     <section className="hero-grid">
       {heroes.map((hero) => (
-        <HeroCard key={hero.id} hero={hero} />
+        <HeroCard key={hero.id} hero={hero} onSelectHero={onSelectHero} />
       ))}
     </section>
   );
