77ae7ce0b791736f28c83f214764536e8f913952
Taron Bentley <taronbentley1@gmail.com>
2026-05-05 19:01:05 -0400

    replace anchor tags with Link components for Home and Battle Arena in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar">
       <h2 className="logo">Multiverse Archive</h2>
 
       <div className="nav-links">
-        <a href="#">Home</a>
-        <a href="#">Battle Arena</a>
-        <a href='#'>Favorites</a>
+        <Link to="/">Home</Link>
+        <Link to="/battle">Battle Arena</Link>
+        <a href="#">Favorites</a>
       </div>
     </nav>
   );
