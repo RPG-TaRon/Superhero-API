a7ee113d6d0e3ad43c77fbd818780c8c3bc4cb06
Taron Bentley <taronbentley1@gmail.com>
2026-05-06 09:29:36 -0400

    implement battle selection logic in App component and pass handler to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Battle from "./pages/Battle";
 
 function App() {
+  const [fighterOne, setFighterOne] = useState(null);
+  const [fighterTwo, setFighterTwo] = useState(null);
+
+  function handleAddToBattle(hero) {
+    if (!fighterOne) {
+      setFighterOne(hero);
+    } else if (!fighterTwo) {
+      setFighterTwo(hero);
+    } else {
+      setFighterOne(hero);
+      setFighterTwo(null);
+    }
+  }
+
   return (
     <>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/battle" element={<Battle />} />
+        <Route
+          path="/"
+          element={<Home onAddToBattle={handleAddToBattle} />}
+        />
+
+        <Route
+          path="/battle"
+          element={
+            <Battle
+              fighterOne={fighterOne}
+              fighterTwo={fighterTwo}
+              setFighterOne={setFighterOne}
+              setFighterTwo={setFighterTwo}
+            />
+          }
+        />
       </Routes>
     </>
   );
